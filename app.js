70777b8ae8183e96eb4482557cba07473008237e
Varshitha <varshitha.lavu@gmail.com>
2023-11-01 18:36:13 -0500

    scoops class added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var scoopsRouter = require('./routes/scoops');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/scoops', scoopsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
